fd7e811017234b01a543dc9e304ebd4af16eb4d4
pankajbisht <pankajbisht031@gmail.com>
2019-11-06 23:23:00 +0530

    save btn add

--- js/editor.js
+++ js/editor.js
@@ -7,48 +7,46 @@ function selectTheme() {
     location.hash = "#" + theme;
 }
 
-
 $(function () {
- 	  "use strict";
+    "use strict";
 
 	   var gbl = null;
-	    window.c = _("#canvas");
-
-	     editor = CodeMirror.fromTextArea(document.getElementById("codeArea"), {
-		       lineNumbers: true,
-		       matchBrackets: true,
-		       continueComments: "Enter",
-		       lineWrapping: true,
-           extraKeys: {
-              "Ctrl-/": "toggleComment",
-              "Ctrl-Space": "autocomplete",
-              "F11": function(cm) {
-                  cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-              },
-              "Esc": function(cm) {
-                  if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
-              }
-          },
-          foldGutter: true,
-          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-          styleActiveLine: true,
-          matchBrackets: true
-	   });
+	   window.c = _("#canvas");
+     editor = CodeMirror.fromTextArea(document.getElementById("codeArea"), {
+		     lineNumbers: true,
+		     matchBrackets: true,
+		     continueComments: "Enter",
+		     lineWrapping: true,
+         extraKeys: {
+             "Ctrl-/": "toggleComment",
+             "Ctrl-Space": "autocomplete",
+             "F11": function(cm) {
+                 cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+             },
+             "Esc": function(cm) {
+                 if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+             }
+         },
+         foldGutter: true,
+         gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+         styleActiveLine: true,
+         matchBrackets: true
+	  });
 
     var choice = (location.hash && location.hash.slice(1)) ||
                (document.location.search &&
                 decodeURIComponent(document.location.search.slice(1)));
-  if (choice) {
-    input.value = choice;
-    editor.setOption("theme", choice);
-  }
+
+    if (choice) {
+        input.value = choice;
+        editor.setOption("theme", choice);
+    }
 
     CodeMirror.on(window, "hashchange", function() {
         var theme = location.hash.slice(1);
+
         if (theme) { input.value = theme; selectTheme(); }
-      });
-
-
+    });
 
     function getScript () {
         $(".script").show();
@@ -60,74 +58,66 @@ $(function () {
         return script;
     }
 
+    function saveScript() {
+        var script = getScript();
+        localStorage.setItem("script", script);
+    }
 
-	function focusOut (e) {
-		e.stopPropagation();
+  	function focusOut (e) {
+  		  e.stopPropagation();
 
-		logClear(); //log clear
-
-      	var script = getScript();
+  		  logClear(); //log clear
+        var script = getScript();
         c.clear(0, 0 , 600, 500);
-
-		localStorage.setItem("script", script);
-
-       	$("head").find('script').remove();
-
+   	 	  localStorage.setItem("script", script);
+      	$("head").find('script').remove();
         $("head").append("<script>" + script + "</script>");
-	}
-
+  	}
 
     function reset () {
         window.location.reload();
     }
 
-
-	function code () {
-		$("canvas").addClass("w3-hide");
+  	function code () {
+  		  $("canvas").addClass("w3-hide");
         $("textarea, .CodeMirror").removeClass("w3-hide");
         $(".CodeMirror").css("width", "100%");
-	}
+  	}
 
-
-	function preview () {
-	    $("textarea, .CodeMirror").addClass("w3-hide");
+  	function preview () {
+  	    $("textarea, .CodeMirror").addClass("w3-hide");
         $("canvas").removeClass("w3-hide");
     }
 
     function tooBox (e) {
-		    var self = $(this);
-		    var text = self.text();
+  	    var self = $(this);
+  		  var text = self.text();
 
         if (text == "Reset") reset();
-		    else if (text == "Code") code();
-		    else if (text == "Preview") preview();
+  		  else if (text == "Save") saveScript();
+        else if (text == "Code") code();
+  		  else if (text == "Preview") preview();
         else if(text == "Run") focusOut(e);
-	}
+  	}
 
+  	function logClear () {
+  		  $(".logs").html("");
+  	}
 
-	function logClear () {
-		$(".logs").html("");
-	}
+  	function init () {
 
+        if (localStorage.getItem("script")) {
+  		      gbl = localStorage.getItem("script");
+  		      $(".script").val(gbl);
+  			    editor.getDoc().setValue(gbl);
+  		      $(".innerbox").trigger("blur");
+  	 	  }
+  	}
 
-	function init () {
-		if(localStorage.getItem("script")) {
-		    gbl = localStorage.getItem("script");
-		    $(".script").val(gbl);
-			editor.getDoc().setValue(gbl);
-		    $(".innerbox").trigger("blur");
-		}
-	}
-
-
-	$(document).ready(function () {
-
-		init();
-
-		editor.setSize(600, 500);
-
-		$("#log-clear").on("click", logClear);
-		//$("div").on("focusout", focusOut);
-		$("#tool").on("click", 'button', tooBox);
-	});
+  	$(document).ready(function () {
+  		  init();
+  		  editor.setSize(600, 500);
+  		  $("#log-clear").on("click", logClear);
+  		  $("#tool").on("click", 'button', tooBox);
+  	});
 });
